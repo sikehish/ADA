e4fd6bb2a8e92d9a6df84e50d44f7f141390bd80
Syed Hisham Akmal <hisham0502@gmail.com>
2023-07-26 11:46:00 +0530

    Added relevant documentation

--- Lab 15/dijkstra.c
+++ Lab 15/dijkstra.c
@@ -4,17 +4,21 @@ void dijkstra(int n, int cost[10][10], int s, int dist[10])
 {
     int i, v, count = 1, min, visited[10];
 
+    // initialising
     for (i = 1; i <= n; i++)
     {
         visited[i] = 0;
-        dist[i] = cost[s][i];
+        dist[i] = cost[s][i]; // if no edge exists between s and i then cost[s][i] = 999
     }
+
     visited[s] = 1;
-    dist[s] = 0;
+    dist[s] = 0; // distance from s to s is 0
 
     while (count < n)
     {
         min = 999;
+
+        // finding the vertex with minimum distance
         for (i = 1; i <= n; i++)
         {
             if (dist[i] < min && !visited[i])
@@ -25,7 +29,8 @@ void dijkstra(int n, int cost[10][10], int s, int dist[10])
         }
         visited[v] = 1;
         count++;
-        for (i = 1; i <= n; i++)
+
+                for (i = 1; i <= n; i++)
         {
             if (dist[i] > dist[v] + cost[v][i])
                 dist[i] = dist[v] + cost[v][i];
